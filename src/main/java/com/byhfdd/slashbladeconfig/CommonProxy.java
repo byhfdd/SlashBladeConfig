14d3dba274a02738f3adf9eafa914e31b6f39f46
byhfdd <s922277170@outlook.com>
2025-08-10 22:16:24 +0800

    hf

--- src/main/java/com/byhfdd/slashbladeconfig/CommonProxy.java
+++ src/main/java/com/byhfdd/slashbladeconfig/CommonProxy.java
@@ -8,19 +8,17 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import mods.flammpfeil.slashblade.SlashBlade;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
 
 
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
         Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
         MyMod.LOG.info(Config.greeting);
         MyMod.LOG.info("SBMMMcompat at version " + Tags.VERSION);
     }
 
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
         GameRegistry.addRecipe(new RecipeCopyBladeNBT());
         GameRegistry.addShapedRecipe(new ItemStack(SlashBlade.bladeNamed),
@@ -33,9 +31,7 @@ public class CommonProxy {
         );
     }
 
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {}
 
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {}
 }
